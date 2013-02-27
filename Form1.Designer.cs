184d87fc36a5534453df9bc58d24c452ff71a490
Acsa Lu <acsa0210@gmail.com>
2013-02-27 16:43:02 +0800

    create project

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Sketcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.canvas.Location = new System.Drawing.Point(12, 89);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1170, 502);
+            this.canvas.TabIndex = 1;
+            this.canvas.TabStop = false;
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUP);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 71);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1194, 632);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Sketcher - Untitled";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sketcher
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private Point startPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            Console.Out.WriteLine("MouseDown on canvas");
+            Console.Out.WriteLine("(" + e.Location.X + ", " + e.Location.Y + ")");
+            
+            Pen drawPen = new Pen(Color.Black, 1);
+            g = canvas.CreateGraphics();
+            startPoint = new Point(e.X, e.Y);
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e) {
+            Console.Out.WriteLine("(" + e.Location.X + ", " + e.Location.Y + ")");
+
+            if (g != null)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    Pen drawPen = new Pen(Color.Black, 1);
+                    Point tempPoint = new Point(e.Location.X, e.Location.Y);
+                    g.DrawLine(drawPen, startPoint, tempPoint);
+                    startPoint = tempPoint;
+                }
+            }
+        }
+
+        private void canvas_MouseUP(object sender, MouseEventArgs e) {
+            Console.Out.WriteLine("MouseDown off canvas");
+            Console.Out.WriteLine("(" + e.Location.X + ", " + e.Location.Y + ")");
+            g.Dispose();
+            g = null;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
